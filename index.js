41c68f08b1dc0f872bd47971523929d46c6cce5f
nicolasnardi404 <151576395+nicolasnardi404@users.noreply.github.com>
2024-01-06 17:22:19 +0100

    add feature to choose max lengh of videos

--- index.js
+++ index.js
@@ -4,21 +4,35 @@ const videoArt = document.getElementById("video-art");
 const randomVideo = document.getElementById("random-video");
 const title = document.getElementById("title");
 const description = document.getElementById("description");
+const selectedLength = document.getElementById("length-video");
+
+let filteredVideos = videoDB; // Initialize filteredVideos with the whole videoDB array
 
 function chooseVideo() {
-  let randomNumber = Math.floor(Math.random() * videoDB.length);
-  let videoScreening = videoDB[randomNumber];
+  let randomNumber = Math.floor(Math.random() * filteredVideos.length);
+  let videoScreening = filteredVideos[randomNumber];
   videoArt.innerHTML = videoScreening.video;
   title.textContent = videoScreening.title;
   description.textContent = videoScreening.description;
 }
 
-randomVideo.addEventListener("click", function () {
-  chooseVideo();
-});
-
+randomVideo.addEventListener("click", chooseVideo);
 chooseVideo();
 
-const fiveLenght = videoDB.filter(function (length) {
-  return length <= 5;
+selectedLength.addEventListener("change", function (event) {
+  let selectedLength = parseInt(event.target.value);
+
+  if (selectedLength === 5) {
+    filteredVideos = videoDB.filter(function (video) {
+      return video.length <= 5;
+    });
+  } else if (selectedLength === 10) {
+    filteredVideos = videoDB.filter(function (video) {
+      return video.length <= 10;
+    });
+  } else if (selectedLength === 11) {
+    filteredVideos = videoDB; // Assign the entire videoDB array
+  }
+
+  chooseVideo();
 });
